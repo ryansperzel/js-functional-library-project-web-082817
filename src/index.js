d8f7a9ec3a875746972dc01192737bf08b7ac526
ian-ramos <ramos.ianc@gmail.com>
2017-10-10 17:57:47 -0400

    finished lab

--- src/index.js
+++ src/index.js
@@ -40,16 +40,18 @@ const fi = (function() {
       return arr;
     },
 
-    reduce: function(list, iteratee, [memo]) {
+    reduce: function(list, iteratee, memo = 0) {
+      debugger;
       if (Array.isArray(list)) {
-        const listCopy = list.slice();
-        let i = -1;
-        let counter = 0;
-        for (const item of listCopy) {
-          i++;
+        for (const item of list) {
+          memo = iteratee(memo, item);
         }
       } else {
+        for (const key in list) {
+          memo = iteratee(memo, list[key]);
+        }
       }
+      return memo;
     },
 
     find: function(list, predicate) {
@@ -109,8 +111,97 @@ const fi = (function() {
       return counter;
     },
 
-    first: function(array) {
-      debugger;
+    first: function(array, n) {
+      if (n === undefined) {
+        return array[0];
+      } else {
+        return array.slice(0, n);
+      }
+    },
+
+    last: function(array, n) {
+      if (n === undefined) {
+        return array[array.length - 1];
+      } else {
+        return array.slice(array.length - n);
+      }
+    },
+
+    compact: function(array) {
+      const arr = [];
+      for (const el of array) {
+        if (!!el) {
+          arr.push(el);
+        }
+      }
+      return arr;
+    },
+
+    helper: function(array) {
+      for (const el of array) {
+        if (typeof el === "object") {
+          fi.helper(el);
+        } else {
+          return el;
+        }
+      }
+    },
+
+    flatten: function(array) {
+      for (const el of array) {
+        if (typeof el === "object") {
+          fi.flatten(el);
+        } else {
+          const arr = [...el];
+        }
+      }
+      return arr;
+      // const arr = [];
+      // debugger;
+      // for (const el of array) {
+      //   if (typeof el === "object") {
+      //     arr.push(fi.helper(el));
+      //   } else {
+      //     arr.push(el);
+      //   }
+      // }
+      // return arr;
+    },
+
+    uniq: function(array) {
+      const arr = [];
+      for (const el of array) {
+        if (!arr.includes(el)) {
+          arr.push(el);
+        }
+      }
+      return arr;
+    },
+
+    keys: function(object) {
+      const arr = [];
+      for (const key in object) {
+        arr.push(key);
+      }
+      return arr;
+    },
+
+    values: function(object) {
+      const arr = [];
+      for (const key in object) {
+        arr.push(object[key]);
+      }
+      return arr;
+    },
+
+    functions: function(object) {
+      const arr = [];
+      for (const key in object) {
+        if (typeof object[key] === "function") {
+          arr.push(key);
+        }
+      }
+      return arr;
     }
   };
 })();
